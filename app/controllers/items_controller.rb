c6e44b00aa524413c08ec4e726db317a63981f79
Thort <yamaxanado@gmail.com>
2017-09-03 09:48:04 +0000

    fix have/want button in additem page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,9 +11,16 @@ class ItemsController < ApplicationController
         hits: 20
       })
       results.each do |result|
-        item = Item.new(read(result))
+        item = Item.find_or_initialize_by(read(result))
         @items << item
       end
     end
   end
+  
+  def show
+    @item = Item.find(params[:id])
+    @want_users = @item.want_users
+    @have_users = @item.have_users
+  end
+  
 end
